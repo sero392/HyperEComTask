e72b28422768c3326bad79ef7fead2e8050ded82
Serhan Çelik <serhann798@gmail.com>
2025-03-28 14:22:52 +0300

    mobiledeki buton görülmeme sorunu çözüldü

--- src/Components/Home/HomePageComp.jsx
+++ src/Components/Home/HomePageComp.jsx
@@ -2,12 +2,13 @@ import { Button, Grid, IconButton, Pagination } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import PaginationComp from "./PaginationComp";
 import { ShoppingCart } from "lucide-solid";
-import { ShoppingCartIcon, View } from "lucide-react";
+import { BaggageClaim, ShoppingCartIcon, View } from "lucide-react";
 import { addBasketItem } from "../../Features/BasketSlice";
 import ProductFilterComp from "./ProductFilterComp";
 export default function HomePageComp({ setLoading }) {
     const dispatch = useDispatch();
     const products = useSelector((state) => state.product.products);
+
     return (
         <>
             <ProductFilterComp setLoading={setLoading}></ProductFilterComp>
@@ -15,7 +16,7 @@ export default function HomePageComp({ setLoading }) {
                 {products.length === 0 ? (
                     <div className="flex justify-center items-center min-h-100">
                         <span className="text-xl font-bold">
-                                Ürün Bulunamadı!
+                            Ürün Bulunamadı!
                         </span>
                     </div>
                 )
@@ -25,19 +26,46 @@ export default function HomePageComp({ setLoading }) {
                             {
                                 products.map((product, index) => {
                                     return (
-                                        <div key={index} className="relative  shadow-md shadow-gray-500/50 h-72
+                                        <div key={index} className="relative  shadow-md shadow-gray-500/50 h-90 sm:h-72
                                      rounded-lg overflow-hidden group">
-                                            <div className="absolute inset-0 transition-all duration-300 group-hover:blur-sm">
+                                            <div className="absolute inset-0 z-1 transition-all duration-300 group-hover:blur-sm ">
                                                 <img src={product?.productData?.productMainImage} alt={product?.productName} className="w-full rounded-t-md  object-cover h-48" />
                                                 <div className="flex justify-center items-center flex-col p-5">
                                                     <span className="font-bold text-md text-blue-400">
                                                         {product?.marketPrice?.toFixed(2)} TL
                                                     </span>
                                                 </div>
+
+                                                <div className="flex sm:hidden justify-center items-center flex-col px-5 pb-5">
+                                                    <Button fullWidth onClick={() => dispatch(addBasketItem(product))}
+                                                        sx={{
+                                                            fontSize: 11,
+                                                            backgroundColor: '#4db6ac',
+                                                            color: 'white',
+                                                        }}
+                                                    >
+                                                        <ShoppingCartIcon size={15}></ShoppingCartIcon>
+                                                        <span className="ml-2">
+                                                            Sepete Ekle
+                                                        </span>
+                                                    </Button>
+                                                    <Button
+                                                           sx={{
+                                                            fontSize: 11,
+                                                            backgroundColor: '#26c6da',
+                                                            color: 'white',
+                                                        }}
+                                                        fullWidth size="small" variant="contained" className="!mt-2">
+                                                        <View size={15}></View>
+                                                        <span className="ml-2">
+                                                            Gör
+                                                        </span>
+                                                    </Button>
+                                                </div>
                                             </div>
-                                            <div className="absolute inset-0 bg-transparent 
+                                            <div  className="absolute inset-0 bg-transparent z-0
                                     text-white flex flex-col items-center justify-center text-md opacity-0 
-                                    transition-opacity duration-300 group-hover:opacity-100 text-center">
+                                    transition-opacity duration-300 group-hover:z-2 group-hover:opacity-100 text-center">
                                                 <span className="text-white p-3 text-sm font-bold">
                                                     {product?.productName}
                                                 </span>
